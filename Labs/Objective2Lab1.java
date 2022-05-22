9a3887d688ecfe44eeff277048d832ffcb333caf
Thao Nguyen <thaontt1408@gmail.com>
2022-05-22 14:36:06 -0600

    complete objective2lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,9 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    System.out.println("Grocery List");
+    System.out.println("1. Milk");
+    System.out.println("2. Egg");
+    System.out.println("3. Bread");
+
+  }
+}
