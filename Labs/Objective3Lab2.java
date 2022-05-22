6ee895ca9c3f783144c3e03827b4b550d3e827f3
Thao Nguyen <thaontt1408@gmail.com>
2022-05-22 15:35:58 -0600

    complete objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,6 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double hypotenuse = Math.sqrt(10*10 + 8*8);
+    System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is "+ hypotenuse);
+  }
+}
